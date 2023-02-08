1136e889db8ef1f07f2fdcd818fd802e69436af5
Sujeong <nksj98@naver.com>
2023-02-08 19:43:09 +0900

    signupPage design

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -27,6 +27,8 @@ const SignUp = () => {
       setDisabled(false);
     }
   };
+  const emailRef = useRef();
+
   const EmailValidation = (e) => {
     setEmail(e.target.value);
 
@@ -76,42 +78,82 @@ const SignUp = () => {
   };
 
   return (
-    <SignupForm>
-      <input
-        required
-        type="email"
-        data-testid="email-input"
-        placeholder="이메일을 입력해 주세요"
-        value={email}
-        onChange={EmailValidation}
-      />
-      <div>{message}</div>
-      <input
-        required
-        type="password"
-        data-testid="password-input"
-        placeholder="비밀번호를 입력해주세요"
-        value={pw}
-        onChange={PwValidation}
-      />
-      <div>{pwmessage}</div>
-      <button data-testid="signup-button" onClick={Sign} disabled={disabled}>
-        회원가입
-      </button>
-      <Link to="/">홈</Link>
-    </SignupForm>
+    <SignUpPage>
+      <h1>SignIn</h1>
+      <SignupForm>
+        <input
+          required
+          type="email"
+          data-testid="email-input"
+          placeholder="이메일을 입력해 주세요"
+          value={email}
+          onChange={EmailValidation}
+          ref={emailRef}
+        />
+        <ErrorMessage>{message}</ErrorMessage>
+        <input
+          required
+          type="password"
+          data-testid="password-input"
+          placeholder="비밀번호를 입력해주세요"
+          value={pw}
+          onChange={PwValidation}
+        />
+        <ErrorMessage>{pwmessage}</ErrorMessage>
+        <button data-testid="signup-button" onClick={Sign} disabled={disabled}>
+          회원가입
+        </button>
+        <OtherLinks>
+          <span>
+            <Link to="/">홈</Link>으로 가기
+          </span>
+        </OtherLinks>
+      </SignupForm>
+    </SignUpPage>
   );
 };
 
 export default SignUp;
 
-const SignupForm = styled.form`
+const SignUpPage = styled.main`
   width: 300px;
+  margin: 150px auto 0;
+  padding: 100px 25px;
+  text-align: center;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const SignupForm = styled.form`
   display: flex;
   flex-direction: column;
 
   input {
     height: 35px;
-    margin: 10px 0;
+    margin: 15px 0;
+    padding-left: 5px;
   }
+
+  button {
+    margin-top: 15px;
+    padding: 10px 0;
+    background-color: #fff;
+    border: 1px solid black;
+    border-radius: 5px;
+    font-weight: 500;
+
+    &:hover {
+      cursor: pointer;
+      background-color: #e9e9e9;
+    }
+  }
+`;
+
+const OtherLinks = styled.section`
+  padding-top: 15px;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 0.8rem;
 `;
